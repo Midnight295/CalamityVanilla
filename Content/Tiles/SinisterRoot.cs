73f0edd53bb589648d8e2b3cded131530b8c5275
Jacob Jacobsphere <geometricwarrior@gmail.com>
2025-04-01 07:50:07 -0500

    Sinister root, spawnrate may need to be adjusted

---
+++ Content/Tiles/SinisterRoot.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameContent.Metadata;
+using Terraria.Localization;
+using Microsoft.Xna.Framework;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.DataStructures;
+
+namespace CalamityVanilla.Content.Tiles
+{
+    public class SinisterRoot : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileObsidianKill[Type] = true;
+            Main.tileCut[Type] = true;
+            Main.tileNoFail[Type] = true;
+            TileID.Sets.ReplaceTileBreakUp[Type] = true;
+            TileID.Sets.IgnoredInHouseScore[Type] = true;
+            TileMaterials.SetForTileId(Type, TileMaterials._materialsByName["Plant"]);
+            LocalizedText name = CreateMapEntryName();
+            AddMapEntry(new Color(124, 87, 149), name);
+
+            TileObjectData.newTile.CoordinateHeights = [22];
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.RandomStyleRange = 3;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.StyleAlch);
+            TileObjectData.newTile.AnchorValidTiles = [
+                TileID.CorruptGrass,
+                TileID.Ebonstone,
+                TileID.Ebonsand,
+                TileID.CorruptSandstone,
+                TileID.CorruptHardenedSand,
+                TileID.CorruptJungleGrass,
+                TileID.CorruptIce,
+            ];
+            TileObjectData.newTile.AnchorAlternateTiles = [
+                TileID.ClayPot,
+                TileID.PlanterBox
+            ];
+            TileObjectData.addTile(Type);
+
+            HitSound = SoundID.Grass;
+            DustType = DustID.Corruption;
+            RegisterItemDrop(ModContent.ItemType<Items.Consumable.HiveMindSummon>());
+        }
+        public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY)
+        {
+            offsetY = -2;
+        }
+        public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref TileDrawInfo drawData)
+        {
+            if (Main.rand.NextBool(5))
+            {
+                Dust d = Dust.NewDustDirect(new Vector2(i * 16, j * 16), 16, 16, DustID.Corruption);
+                d.velocity.X *= 0.1f;
+                d.velocity.Y = Main.rand.NextFloat(-1f, 0f);
+                d.alpha = 128;
+            }
+        }
+    }
+    public class SinisterRootSpanwer : GlobalTile
+    {
+        public override void RandomUpdate(int i, int j, int type)
+        {
+            if (type != TileID.CorruptGrass && type != TileID.Ebonstone && type != TileID.Ebonsand && type != TileID.CorruptSandstone && type != TileID.CorruptHardenedSand && type != TileID.CorruptJungleGrass && type != TileID.CorruptIce)
+                return;
+            if (Main.tile[i, j-1].TileType == ModContent.TileType<SinisterRoot>() || !Main.hardMode || !Main.rand.NextBool(100))
+                return;
+            WorldGen.PlaceTile(i, j - 1, ModContent.TileType<SinisterRoot>(), style: Main.rand.Next(3));
+        }
+    }
+}
