658ebe916095980a1bb4240e26317fc0364d4a04
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-08-28 15:27:06 +0300

    mostly done perfect dark

---
+++ Content/Items/Weapons/Melee/PerfectDark.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityVanilla.Content.Items.Weapons.Melee
+{
+    public class PerfectDark : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.Size = new Vector2(32, 32);
+            Item.damage = 45;
+            Item.knockBack = 3f;
+            Item.useTime = 27;
+            Item.useAnimation = 27;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.UseSound = SoundID.Item1;
+            Item.DamageType = DamageClass.Melee;
+            Item.rare = ItemRarityID.Pink;
+            Item.value = Item.sellPrice(0, 2, 0, 0);
+            Item.shoot = ModContent.ProjectileType<Projectiles.Melee.PerfectDarkCloud>();
+            Item.shootSpeed = 5;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            int cloudAmount = Main.rand.Next(2, 5 + 1);
+            for (int i = 0; i < cloudAmount; i++)
+            {
+                Projectile.NewProjectile
+                (
+                    source,
+                    position,
+                    velocity.RotatedByRandom(0.2f) * Main.rand.NextFloat(0.66f, 1.5f),
+                    type,
+                    (int)(damage * 0.5f),
+                    knockback,
+                    player.whoAmI,
+                    ai0: Main.rand.Next(90, 150)
+                );
+            }
+            return false;
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                float pointProgress = Main.rand.NextFloat(0.6f);
+                CVUtils.GetPointOnSwungItemPath(68f, 84f, 0.4f + pointProgress, player.GetAdjustedItemScale(Item), out var location, out var outwardDirection, player);
+                Vector2 velocity = outwardDirection.RotatedBy(MathHelper.PiOver2 * player.direction * player.gravDir);
+
+                float dustInterpolator = Utils.GetLerpValue(0.2f, 0.4f, pointProgress, true);
+                float dustScale = MathHelper.Lerp(1.5f, 2.5f, dustInterpolator);
+                Color dustColor = Color.Lerp(Color.White, Color.Black, dustInterpolator);
+                int dustAlpha = 0;// (int)MathHelper.Lerp(100, 0, dustInterpolator);
+                int dustType = DustID.Shadowflame;
+
+                Dust dust = Dust.NewDustPerfect(
+                    location,
+                    dustType,
+                    velocity * 4f,
+                    Alpha: dustAlpha,
+                    newColor: dustColor,
+                    Scale: dustScale
+                );
+                dust.noGravity = true;
+            }
+        }
+    }
+}
---
+++ Content/Projectiles/Melee/PerfectDarkCloud.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityVanilla.Content.Projectiles.Melee
+{
+    public class PerfectDarkCloud : ModProjectile
+    {
+        public float TotalTime => Projectile.ai[0];
+        public ref float Timer => ref Projectile.localAI[0];
+
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Type] = 3;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 40;
+            Projectile.height = 40;
+
+            Projectile.penetrate = -1;
+            Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 20;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+
+            Projectile.frame = Main.rand.Next(3);
+            Projectile.rotation = Main.rand.NextFloat(MathHelper.TwoPi);
+        }
+
+        public override void AI()
+        {
+            Timer++;
+
+            if (Timer % 3 == 0)
+            {
+                Vector2 velocity = Projectile.velocity.RotatedByRandom(0.2) * Main.rand.NextFloat(2f, 3f);
+                Dust dust = Dust.NewDustDirect(
+                    Projectile.position, Projectile.width, Projectile.height,
+                    DustID.Obsidian,
+                    0, 0,
+                    Alpha: 100,
+                    newColor: Color.White,
+                    Scale: 1f
+                );
+                dust.noGravity = true;
+            }
+
+            Projectile.Opacity = Utils.GetLerpValue(0, 10, Timer, true) * Utils.GetLerpValue(TotalTime, TotalTime - 20, Timer, true);
+            if (Timer > TotalTime)
+            {
+                Projectile.Kill();
+                return;
+            }
+
+            Projectile.velocity *= 0.98f;
+            Projectile.rotation += Projectile.velocity.X * 0.02f;
+        }
+    }
+}
