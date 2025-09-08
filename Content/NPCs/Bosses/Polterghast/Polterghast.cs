5839bb96d630d333ae3be2c92e170125d4e180fc
Scratch Lunin <scratchoc2017@gmail.com>
2025-09-07 22:22:30 -0700

    improved polter visuals

--- Content/NPCs/Bosses/Polterghast/Polterghast.cs
+++ Content/NPCs/Bosses/Polterghast/Polterghast.cs
@@ -18,7 +18,7 @@ namespace CalamityVanilla.Content.NPCs.Bosses.Polterghast
         public byte phase = 0;
         public Player target => Main.player[NPC.target];
 
-        private static Asset<Texture2D> spikeBall, spikeChain;
+        private static Asset<Texture2D> spikeBall, spikeChain, trail;
         public override bool CanHitPlayer(Player target, ref int cooldownSlot)
         {
             if (NPC.alpha > 64)
@@ -42,6 +42,7 @@ namespace CalamityVanilla.Content.NPCs.Bosses.Polterghast
         {
             spikeBall = ModContent.Request<Texture2D>(Texture + "Spike");
             spikeChain = ModContent.Request<Texture2D>(Texture + "SpikeChain");
+            trail = ModContent.Request<Texture2D>(Texture + "_Glow");
 
             NPCID.Sets.TrailCacheLength[Type] = 10; // The length of old position to be recorded
             NPCID.Sets.TrailingMode[Type] = 1; // The recording mode
@@ -91,12 +92,12 @@ namespace CalamityVanilla.Content.NPCs.Bosses.Polterghast
             NPC.CloneDefaults(NPCID.EyeofCthulhu);
 
             NPC.lifeMax = 30000;
-            NPC.defense = 30;
+            NPC.defense = 18;
 
             NPC.aiStyle = -1;
             NPC.behindTiles = false;
             NPC.noGravity = true;
-            phase = 0;
+            phase = 1;
             Music = MusicID.Boss4;
             NPC.Size = new Vector2(90);
             NPC.noTileCollide = true;
@@ -115,9 +116,11 @@ namespace CalamityVanilla.Content.NPCs.Bosses.Polterghast
 
         public override void AI()
         {
-            for (int i = 0; i < 5; i++)
+            if (NPC.life < NPC.lifeMax * 0.75f) { phase = 2; }
+
+            for (int i = 0; i < 2; i++)
             {
-                Dust d = Dust.NewDustDirect(NPC.position, NPC.width, NPC.height, DustID.DungeonSpirit);
+                Dust d = Dust.NewDustDirect(NPC.position + new Vector2(NPC.width/4 + NPC.width/2 * -NPC.direction, NPC.width*1/8), NPC.width/2, NPC.height*3/4, DustID.DungeonSpirit);
                 d.noGravity = true;
                 d.scale = Main.rand.NextFloat(1f, 2f);
                 d.velocity = NPC.velocity/2f;
@@ -158,26 +161,27 @@ namespace CalamityVanilla.Content.NPCs.Bosses.Polterghast
             Asset<Texture2D> tex = TextureAssets.Npc[Type];
             NPC.ai[1]++;
 
+            // AFTERIMAGE TRAIL
             SpriteEffects spriteEffects = NPC.spriteDirection == -1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
             Vector2 drawOrigin = new Vector2(NPC.width, NPC.height);
             for (int k = 0; k < NPC.oldPos.Length; k++)
             {
-                //Main.NewText(NPC.oldPos.Length);
                 Vector2 drawPos = NPC.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(-50f, -15);
-                Color color = NPC.GetAlpha(drawColor) * ((float)(NPC.oldPos.Length - k) / (float)NPC.oldPos.Length) * 0.5f;
-                spriteBatch.Draw(tex.Value, drawPos, NPC.frame, color, NPC.rotation, drawOrigin, NPC.scale - k / (float)NPC.oldPos.Length / 3, spriteEffects, 0f);
+                Color color = Color.White * ((float)(NPC.oldPos.Length - k) / (float)NPC.oldPos.Length) * 0.25f;
+                spriteBatch.Draw(trail.Value, drawPos, trail.Frame(1, 3, 0, NPC.frame.Y/116), color, NPC.rotation, drawOrigin, NPC.scale - k / (float)NPC.oldPos.Length / 3, spriteEffects, 0f);
             }
 
-            for (int i = 1; i < 4; i++)
+            // SPIKY BALLS/CHAINS
+            Color lightColor;
+            float spinSpeed = phase == 2 ? 300f : 40f; // the smaller the number, the faster the spike balls spin
+            Rectangle ballRect = new Rectangle(0, 0, 52, 54);
+            Rectangle chainRect = new Rectangle(0, 0, 14, 14);
+            int chainCount = 5;
+            int ballCount = 3;
+            for (int i = 1; i < ballCount + 1; i++)
             {
-                float spinSpeed = 35f; // the smaller the number, the faster the spike balls spin
-                Vector2 ballDist = (Vector2.One * (130f + (float)Math.Sin(NPC.ai[1] / 50f) * 35f)).RotatedBy(NPC.ai[1] / spinSpeed).RotatedBy(MathHelper.TwoPi / 3f * i);
+                Vector2 ballDist = (Vector2.One * (130f + (float)Math.Sin(NPC.ai[1] / 50f) * 35f)).RotatedBy(NPC.ai[1] / spinSpeed).RotatedBy(MathHelper.TwoPi / ballCount * i);
                 Vector2 ballPos = NPC.Center - Main.screenPosition + ballDist;
-                Rectangle ballRect = new Rectangle(0, 0, 52, 54);
-                Rectangle chainRect = new Rectangle(0, 0, 14, 14);
-                int chainCount = 5;
-                Color lightColor;
-
                 for (int j = 1; j < chainCount + 1; j++)
                 {
                     Vector2 chainPos = ballDist / (chainCount + 1) * j + NPC.Center - Main.screenPosition;
@@ -188,10 +192,12 @@ namespace CalamityVanilla.Content.NPCs.Bosses.Polterghast
                 spriteBatch.Draw(spikeBall.Value, ballPos, ballRect, lightColor, 0, spikeBall.Size() / 2, NPC.scale, SpriteEffects.None, 0);
             }
 
-            //spriteBatch.Draw(backTexture.Value, NPC.Center - Main.screenPosition, smallFlake, Color.White * 0.7f, -NPC.rotation, smallFlake.Size() / 2, !ForTheWorthy ? 1f : 2f, SpriteEffects.None, 0);
-
-            spriteBatch.Draw(tex.Value, NPC.Center - Main.screenPosition, NPC.frame, Color.White, NPC.rotation, NPC.frame.Size() / 2, NPC.scale, NPC.direction == 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
-
+            // MAIN BODY SPRITE
+            lightColor = Lighting.GetColor((NPC.Center).ToTileCoordinates());
+            spriteBatch.Draw(tex.Value, NPC.Center - Main.screenPosition, NPC.frame, lightColor * (phase == 2 ? 0.1f : 1f), NPC.rotation, NPC.frame.Size() / 2, NPC.scale, NPC.direction == 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
+            
+            // GLOWMASK
+            spriteBatch.Draw(trail.Value, NPC.Center - Main.screenPosition, trail.Frame(1, 3, 0, NPC.frame.Y / 116), Color.White, NPC.rotation, NPC.frame.Size() / 2, NPC.scale, spriteEffects, 0f);
 
             if (NPC.IsABestiaryIconDummy)
             {
